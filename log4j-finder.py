85eab6831850c498cd38cfae51b607a4e822a39a
B3K7 <79159661+B3K7@users.noreply.github.com>
2021-12-18 16:03:37 -0500

    Update log4j-finder.py
    restored proper JndiManager.class MD5 ordering

--- log4j-finder.py
+++ log4j-finder.py
@@ -71,7 +71,6 @@ FILENAMES = [
 # Known BAD
 MD5_BAD = { "lookup/StrSubstitutor.class" : {
       # StrSubstitutor.class (source: https://logging.apache.org/log4j/2.x/security.html#CVE-2021-45105 )
-      # Partial list - please validate all md5 sums
 
       "0d6a71c70eff31cc1a4e966666cdf473": "log4j 2.14.0 ",
       "1ac1fbb3d633a7e20608b3112c78ae34": "log4j 2.14.1 - 2.16.0 ",
@@ -91,19 +90,19 @@ MD5_BAD = { "lookup/StrSubstitutor.class" : {
     }, "lookup/JndiManager.class" : {
       # JndiManager.class (source: https://github.com/nccgroup/Cyber-Defence/blob/master/Intelligence/CVE-2021-44228/modified-classes/md5sum.txt)
       "04fdd701809d17465c17c7e603b1b202": "log4j 2.9.0 - 2.11.2",
-      "102cac5b7726457244af1f44e54ff468": "log4j 2.12.2",
       "21f055b62c15453f0d7970a9d994cab7": "log4j 2.13.0 - 2.13.3",
       "3bd9f41b89ce4fe8ccbf73e43195a5ce": "log4j 2.6 - 2.6.2",
       "415c13e7c8505fb056d540eac29b72fa": "log4j 2.7 - 2.8.1",
       "5824711d6c68162eb535cc4dbf7485d3": "log4j 2.12.0 - 2.12.1",
-      # 2.15.0 vulnerable to Denial of Service attack (source: https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-45046)
-      "5d253e53fa993e122ff012221aa49ec3": "log4j 2.15.0",
+      "102cac5b7726457244af1f44e54ff468": "log4j 2.12.2",
       "6b15f42c333ac39abacfeeeb18852a44": "log4j 2.1 - 2.3",
       "8b2260b1cce64144f6310876f94b1638": "log4j 2.4 - 2.5",
       "a193703904a3f18fb3c90a877eb5c8a7": "log4j 2.8.2",
+      "f1d630c48928096a484e4b95ccb162a0": "log4j 2.14.0 - 2.14.1",
+      # 2.15.0 vulnerable to Denial of Service attack (source: https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-45046)
+      "5d253e53fa993e122ff012221aa49ec3": "log4j 2.15.0",
       # 2.16.0 vulnerable to Infinite recursion in lookup evaluation (source: https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-45105)
       "ba1cf8f81e7b31c709768561ba8ab558": "log4j 2.16.0",
-      "f1d630c48928096a484e4b95ccb162a0": "log4j 2.14.0 - 2.14.1",
     },
 }
 
